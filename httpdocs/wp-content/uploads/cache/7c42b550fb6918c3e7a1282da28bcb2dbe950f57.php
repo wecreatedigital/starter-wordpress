d6f7ea2a95e88e30917415d70b020dfc79048d6c
brandon <brandon@wecreatedigital.co.uk>
2019-04-05 17:05:02 +0100

    404 page

--- httpdocs/wp-content/uploads/cache/7c42b550fb6918c3e7a1282da28bcb2dbe950f57.php
+++ httpdocs/wp-content/uploads/cache/7c42b550fb6918c3e7a1282da28bcb2dbe950f57.php
@@ -1,13 +1,21 @@
 <?php $__env->startSection('content'); ?>
-  <?php echo $__env->make('partials.page-header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <div class="row d-flex justify-content-center">
 
+  <h1 class="display-1 ">404</h1>
+  </div>
   <?php if(!have_posts()): ?>
-    <div class="alert alert-warning">
-      <?php echo e(__('Sorry, but the page you were trying to view does not exist.', 'sage')); ?>
+      <div class="row">
+          <div class="col-md-8">
+                <div class="h3 alert alert-warning">
+                  <?php echo e(__('Sorry, an error has occured, Requested page not found!', 'sage')); ?>
 
+                </div>
+            </div>
+            <div class="col-md-4 mt-3">
+                <?php echo get_search_form(false); ?>
+
+            </div>
     </div>
-    <?php echo get_search_form(false); ?>
-
   <?php endif; ?>
 <?php $__env->stopSection(); ?>
 
