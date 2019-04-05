aedd6c6569714e5c3e74a040a1bde3b504a1fe4f
brandon <brandon@wecreatedigital.co.uk>
2019-04-05 13:24:24 +0100

    Added fontpage.blade.php view

---
+++ httpdocs/wp-content/themes/laravel-theme/resources/views/fontpage.blade.php
@@ -0,0 +1,12 @@
+{{--
+  Template Name: Fontpage
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.page-header')
+    @include('partials.content-page')
+  @endwhile
+@endsection
