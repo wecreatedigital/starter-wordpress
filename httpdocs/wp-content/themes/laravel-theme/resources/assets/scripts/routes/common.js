7076c40bdab94e6a9100d08b3b88ac8309a9282d
brandon <brandon@wecreatedigital.co.uk>
2019-04-05 13:14:57 +0100

    Added slick Slider

--- httpdocs/wp-content/themes/laravel-theme/resources/assets/scripts/routes/common.js
+++ httpdocs/wp-content/themes/laravel-theme/resources/assets/scripts/routes/common.js
@@ -1,6 +1,11 @@
+import 'slick-carousel';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // Slick Carousel
+      $('.slick-carousel').slick();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
