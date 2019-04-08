53a90394163ec0a7678b4cd45556e0ed9e345e3f
brandon <brandon@wecreatedigital.co.uk>
2019-04-08 16:50:09 +0100

    Updates

--- httpdocs/wp-content/uploads/cache/7c42b550fb6918c3e7a1282da28bcb2dbe950f57.php
+++ httpdocs/wp-content/uploads/cache/7c42b550fb6918c3e7a1282da28bcb2dbe950f57.php
@@ -1,20 +1,23 @@
 <?php $__env->startSection('content'); ?>
     <div class="row d-flex justify-content-center">
 
-  <h1 class="display-1 ">404</h1>
+  <h1 class="display-1 jumbotron w-100 text-center">404</h1>
   </div>
   <?php if(!have_posts()): ?>
       <div class="row">
-          <div class="col-md-8">
+          <div class="col-md">
                 <div class="h3 alert alert-warning">
                   <?php echo e(__('Sorry, an error has occured, Requested page not found!', 'sage')); ?>
 
                 </div>
             </div>
-            <div class="col-md-4 mt-3">
-                <?php echo get_search_form(false); ?>
 
-            </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 offset-md-6 mt-3">
+            <?php echo get_search_form(false); ?>
+
+        </div>
     </div>
   <?php endif; ?>
 <?php $__env->stopSection(); ?>
--- httpdocs/wp-content/uploads/cache/d0a54976b3d6cf3fc382ee37c0240d30feef38e8.php
+++ httpdocs/wp-content/uploads/cache/d0a54976b3d6cf3fc382ee37c0240d30feef38e8.php
@@ -2,8 +2,17 @@
   <?php while(have_posts()): ?> <?php the_post() ?>
     <?php echo $__env->make('partials.page-header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
      <?php echo $__env->make('partials.content-page', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-     <?php echo $__env->make('partials.google-map', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+
+     <p class="test">Here is some text</p>
+
+     
+
   <?php endwhile; ?>
+  <script>
+    document.addEventListener( 'wpcf7mailsent', function( event ) {
+        location = '<?php echo the_field('contact_form_redirect_url'); ?>' ;
+    }, false );
+    </script>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
