a464e1168ba7819a084571c23a333cd8b6f94e37
brandon <brandon@wecreatedigital.co.uk>
2019-04-09 10:56:53 +0100

    Options page and lib/media

--- httpdocs/wp-content/themes/laravel-theme/resources/lib/general.php
+++ httpdocs/wp-content/themes/laravel-theme/resources/lib/general.php
@@ -8,11 +8,6 @@ if (function_exists('acf_add_options_page')) {
 }
 
 /**
- * Image sizes https://developer.wordpress.org/reference/functions/add_image_size/
- */
-//add_image_size( 'image_size_name', 120, 240, false);
-
-/**
  * Custom footer menu https://codex.wordpress.org/Function_Reference/register_nav_menus
  */
 register_nav_menus([
