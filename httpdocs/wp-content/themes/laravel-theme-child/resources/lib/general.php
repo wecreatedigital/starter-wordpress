29f8c130712c4a09ca8c9143db525340e8548eb4
Dean Appleton-Claydon <dac@Deans-MacBook-Pro.local>
2019-08-21 20:59:54 +0100

    Clean up styles and scripts

--- httpdocs/wp-content/themes/laravel-theme-child/resources/lib/general.php
+++ httpdocs/wp-content/themes/laravel-theme-child/resources/lib/general.php
@@ -25,31 +25,62 @@ add_action('send_headers', 'send_frame_options_header', 10, 0);
 function remove_excess_links_etc()
 {
     remove_action('wp_head', 'feed_links_extra', 3);                    // Display the links to the extra feeds such as category feeds
-  remove_action('wp_head', 'feed_links', 2);                          // Display the links to the general feeds: Post and Comment Feed
-  remove_action('wp_head', 'rsd_link');                               // Display the link to the Really Simple Discovery service endpoint, EditURI link
-  remove_action('wp_head', 'wlwmanifest_link');                       // Display the link to the Windows Live Writer manifest file.
-  remove_action('wp_head', 'index_rel_link');                         // index link
-  remove_action('wp_head', 'parent_post_rel_link', 10, 0);            // prev link
-  remove_action('wp_head', 'start_post_rel_link', 10, 0);             // start link
-  remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);         // Display relational links for the posts adjacent to the current post.
-  remove_action('wp_head', 'wp_generator');                           // Display the XHTML generator that is generated on the wp_head hook, WP version
+    remove_action('wp_head', 'feed_links', 2);                          // Display the links to the general feeds: Post and Comment Feed
+    remove_action('wp_head', 'rsd_link');                               // Display the link to the Really Simple Discovery service endpoint, EditURI link
+    remove_action('wp_head', 'wlwmanifest_link');                       // Display the link to the Windows Live Writer manifest file.
+    remove_action('wp_head', 'index_rel_link');                         // index link
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0);            // prev link
+    remove_action('wp_head', 'start_post_rel_link', 10, 0);             // start link
+    remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);         // Display relational links for the posts adjacent to the current post.
+    remove_action('wp_head', 'wp_generator');                           // Display the XHTML generator that is generated on the wp_head hook, WP version
 
-  remove_action('wp_head', 'rest_output_link_wp_head', 10);           // Remove the REST API lines from the HTML Header
-  remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);      // Remove the REST API lines from the HTML Header
-  remove_action('rest_api_init', 'wp_oembed_register_route');         // Remove the REST API endpoint.
-  add_filter('embed_oembed_discover', '__return_false');              // Turn off oEmbed auto discovery.
-  remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);   // Don't filter oEmbed results.
-  remove_action('wp_head', 'wp_oembed_add_host_js');                  // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action('wp_head', 'rest_output_link_wp_head', 10);           // Remove the REST API lines from the HTML Header
+    remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);      // Remove the REST API lines from the HTML Header
+    remove_action('rest_api_init', 'wp_oembed_register_route');         // Remove the REST API endpoint.
+    add_filter('embed_oembed_discover', '__return_false');              // Turn off oEmbed auto discovery.
+    remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);   // Don't filter oEmbed results.
+    remove_action('wp_head', 'wp_oembed_add_host_js');                  // Remove oEmbed-specific JavaScript from the front-end and back-end.
 
-  // Filters for WP-API version 1.x
+    // Filters for WP-API version 1.x
     add_filter('json_enabled', '__return_false');
     add_filter('json_jsonp_enabled', '__return_false');
 
     // Filters for WP-API version 2.x
     add_filter('rest_enabled', '__return_false');
     add_filter('rest_jsonp_enabled', '__return_false');
+
+    // Remove emoji scripts
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
 }
 add_action('after_setup_theme', 'remove_excess_links_etc');
+
+/**
+ * Remove comment styles
+ *
+ * @author Dean Appleton-Claydon
+ * @date   2019-08-21
+ */
+function remove_recent_comments_style()
+{
+    global $wp_widget_factory;
+    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+}
+add_action('widgets_init', 'remove_recent_comments_style');
+
+/**
+ * Remove the Gutenburg styles
+ *
+ * @author Dean Appleton-Claydon
+ * @date   2019-08-21
+ */
+function remove_block_css()
+{
+    wp_dequeue_style('wp-block-library');
+}
+add_action('wp_enqueue_scripts', 'remove_block_css', 100);
 
 /**
  * Disable pingback http://marketingwithvladimir.com/3-methods-disable-xmlrpc-pingback-wordpress/
