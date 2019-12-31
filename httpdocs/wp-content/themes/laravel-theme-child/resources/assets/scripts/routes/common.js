0891b4d9bbc9b2092247c6c885e9c6990318d7a7
Dean Claydon <dean@wecreatedigital.co.uk>
2019-12-31 07:16:54 +0000

    Enhancement: scrollto animation for both on page ids and hash in URL

--- httpdocs/wp-content/themes/laravel-theme-child/resources/assets/scripts/routes/common.js
+++ httpdocs/wp-content/themes/laravel-theme-child/resources/assets/scripts/routes/common.js
@@ -2,7 +2,28 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    function scrollToHash(target){
+      if( target.length ) {
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top,
+        }, 750);
+      }
+    }
 
+    if(window.location.hash) {
+      scrollToHash($(window.location.hash));
+    }
+
+    // For accessibility reasons use button if target is on same page, see below function
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault;
+      scrollToHash($(this.getAttribute('href')));
+    });
+
+    $('button[data-target]').on('click', function(event) {
+      event.preventDefault;
+      scrollToHash($(this.getAttribute('data-target')));
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
