4b7840905fbe0fc3880d8bde00629bcdd51f0766
Dean Claydon <dean@wecreatedigital.co.uk>
2020-04-09 13:01:54 +0100

    Jetpack fixed

--- web/app/themes/laravel-theme-child/app/lib/speed.php
+++ web/app/themes/laravel-theme-child/app/lib/speed.php
@@ -84,10 +84,7 @@ function remove_excess_links_etc()
 
     // Filters for WP-API version 1.x
     add_filter('json_enabled', '__return_false');
-    add_filter('json_jsonp_enabled', '__return_false');
-
-    // Filters for WP-API version 2.x
-    add_filter('rest_jsonp_enabled', '__return_false');
+    add_filter('json_jsonp_enabled', '__return_false')
 
     // Remove emoji scripts
     remove_action('wp_head', 'print_emoji_detection_script', 7);
