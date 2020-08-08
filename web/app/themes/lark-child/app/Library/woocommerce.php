3142fe12479608cd6487ad2abcea7b8dfc20d069
Dean Claydon <dean@wecreatedigital.co.uk>
2020-08-08 11:58:02 +0100

    Added rules to exclude WooCommerce CSS/JS files

--- web/app/themes/lark-child/app/Library/woocommerce.php
+++ web/app/themes/lark-child/app/Library/woocommerce.php
@@ -20,8 +20,64 @@ function lark_starter_add_woocommerce_support()
     add_theme_support('wc-product-gallery-lightbox');
     add_theme_support('wc-product-gallery-slider');
 }
-
 add_action('after_setup_theme', 'lark_starter_add_woocommerce_support');
+
+/**
+* Manage WooCommerce styles and scripts.
+*/
+function grd_woocommerce_script_cleaner()
+{
+    // Remove the generator tag
+    remove_action('wp_head', array($GLOBALS['woocommerce'], 'generator'));
+
+    wp_dequeue_style('wc-block-style'); // We never use these blocks
+
+    // Unless we're in the store, remove all the cruft!
+    if ( ! is_woocommerce() && ! is_cart() && ! is_checkout()) {
+        wp_dequeue_style('woocommerce_frontend_styles');
+        wp_dequeue_style('woocommerce-general');
+        wp_dequeue_style('woocommerce-layout');
+        wp_dequeue_style('woocommerce-smallscreen');
+        wp_dequeue_style('woocommerce_fancybox_styles');
+        wp_dequeue_style('woocommerce_chosen_styles');
+        wp_dequeue_style('woocommerce_prettyPhoto_css');
+        wp_dequeue_script('selectWoo');
+        wp_deregister_script('selectWoo');
+        wp_dequeue_script('wc-add-payment-method');
+        wp_dequeue_script('wc-lost-password');
+        wp_dequeue_script('wc_price_slider');
+        wp_dequeue_script('wc-single-product');
+        wp_dequeue_script('wc-add-to-cart');
+        wp_dequeue_script('wc-cart-fragments');
+        wp_dequeue_script('wc-credit-card-form');
+        wp_dequeue_script('wc-checkout');
+        wp_dequeue_script('wc-add-to-cart-variation');
+        wp_dequeue_script('wc-single-product');
+        wp_dequeue_script('wc-cart');
+        wp_dequeue_script('wc-chosen');
+        wp_dequeue_script('woocommerce');
+        wp_dequeue_script('prettyPhoto');
+        wp_dequeue_script('prettyPhoto-init');
+        wp_dequeue_script('jquery-blockui');
+        wp_dequeue_script('jquery-placeholder');
+        wp_dequeue_script('jquery-payment');
+        wp_dequeue_script('fancybox');
+        wp_dequeue_script('jqueryui');
+    }
+}
+add_action('wp_enqueue_scripts', 'grd_woocommerce_script_cleaner', 99);
+
+function woocommerce_dequeue_styles($enqueue_styles)
+{
+    if ( ! is_woocommerce() && ! is_cart() && ! is_checkout()) {
+        unset($enqueue_styles['woocommerce-general']);
+        unset($enqueue_styles['woocommerce-layout']);
+        unset($enqueue_styles['woocommerce-smallscreen']);
+    }
+
+    return $enqueue_styles;
+}
+add_filter('woocommerce_enqueue_styles', 'woocommerce_dequeue_styles');
 
 /**
  * Add revisions to Products CPT
