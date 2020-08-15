b151007be9864e90d5363647406353ec198c09e0
Dean Claydon <dean@wecreatedigital.co.uk>
2020-08-15 18:01:06 +0100

    Fix local setup

--- config/environments/local.php
+++ config/environments/local.php
@@ -8,8 +8,8 @@ use Roots\WPConfig\Config;
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
-Config::define('WP_DEBUG_LOG', true);
-Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('WP_DEBUG_LOG', false);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', false);
 Config::define('SCRIPT_DEBUG', true);
 
 ini_set('display_errors', '1');
