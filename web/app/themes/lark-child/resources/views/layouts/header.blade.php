0bd92745a0ad12751f0a40729b288eb3b9322d33
Dean Claydon <dean@wecreatedigital.co.uk>
2020-08-31 17:24:39 +0100

    Enhancement: detect and change CTA to a button if URL starts with hash

--- web/app/themes/lark-child/resources/views/layouts/header.blade.php
+++ web/app/themes/lark-child/resources/views/layouts/header.blade.php
@@ -25,16 +25,20 @@
         @php
           $header_cta = get_field('header_call_to_action_link', 'option');
         @endphp
-        @if( $header_cta )
+        @if( $header_cta && substr( $header_cta['url'], 0, 4 ) === "http" )
           <a class="d-block btn btn-dark order-1 order-sm-1 order-md-2" href="{{ $header_cta['url'] }}">
             {{ $header_cta['title'] }}
           </a>
+        @elseif( $header_cta )
+          <button data-target="{{ $header_cta['url'] }}" class="d-block btn btn-dark order-1 order-sm-1 order-md-2">
+            {{ $header_cta['title'] }}
+          </button>
         @endif
         {!! wp_nav_menu( array(
           'theme_location'    => 'primary_navigation',
           'depth'             => 1,
           'container'         => 'div',
-          'container_class'   => 'mobile-first-menu',
+          'container_class'   => 'mobile-first-menu order-2 order-sm-2 order-md-1',
           'container_id'      => '',
           'menu_class'        => 'nav d-flex',
           'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
