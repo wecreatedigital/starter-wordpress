6ba7673ddf22d26eb118507b6e78c54823437ffe
Christopher Kelker <chris_j_kelker@hotmail.com>
2021-01-22 16:19:22 +0000

    Update cpt.php
    Making use of Laravel's helpers to write readable Custom Post Type labels dynamically

--- web/app/themes/lark-child/app/Library/cpt.php
+++ web/app/themes/lark-child/app/Library/cpt.php
@@ -1,10 +1,12 @@
 <?php
 
+use Illuminate\Support\Str;
+
 /**
  * Register Custom Post types and their taxonomies here...
  *
  * @author Christopher Kelker 10-01-2020
- * @return array void
+ * @return void
  */
 
 $taxonomies = [
@@ -39,14 +41,14 @@ $cpts = [
  */
 foreach ($taxonomies as $taxonomy => $post_types) {
     add_action('init', function () use ($post_types, $taxonomy) {
-        $taxonomy_plural = str_plural($taxonomy);
-        $uc_taxonomy_plural = ucwords(str_replace('-', ' ', $taxonomy_plural));
-        $uc_taxonomy_singular = ucwords(str_replace('-', ' ', $taxonomy));
+        $taxonomy_plural = str_replace('-', ' ', $taxonomy);
+        $uc_taxonomy_plural = Str::of($taxonomy_plural)->plural()->title();
+        $uc_taxonomy_singular = Str::of($taxonomy_plural)->singular()->title();
 
         // Labels part for the GUI
         $labels = array(
-            'name' => _x($uc_taxonomy_plural, 'taxonomy general name'),
-            'singular_name' => _x('{$uc_taxonomy_singular}', 'taxonomy singular name'),
+            'name' => __($uc_taxonomy_plural, 'taxonomy general name'),
+            'singular_name' => __($uc_taxonomy_singular, 'taxonomy singular name'),
             'search_items' => __("Search {$uc_taxonomy_plural}"),
             'popular_items' => __("Popular {$uc_taxonomy_plural}"),
             'all_items' => __("All {$uc_taxonomy_plural}"),
@@ -59,7 +61,7 @@ foreach ($taxonomies as $taxonomy => $post_types) {
             'separate_items_with_commas' => __("Separate {$taxonomy_plural} with commas"),
             'add_or_remove_items' => __("Add or remove {$taxonomy_plural}"),
             'choose_from_most_used' => __("Choose from the most used {$taxonomy_plural}"),
-            'menu_name' => __($uc_taxonomy_plural),
+            'menu_name' => $uc_taxonomy_plural,
         );
 
         // Now register the non-hierarchical taxonomy like tag
@@ -70,7 +72,7 @@ foreach ($taxonomies as $taxonomy => $post_types) {
             'show_admin_column' => true,
             'update_count_callback' => '_update_post_term_count',
             'query_var' => true,
-            'rewrite' => array('slug' => $taxonomy),
+            'rewrite' => ['slug' => $taxonomy],
         ]);
     }, 0);
 }
@@ -79,18 +81,17 @@ foreach ($taxonomies as $taxonomy => $post_types) {
  * NOW REGISTER CPTs
  */
 foreach ($cpts as $cpt => $cpt_options) {
-
     // Register Custom Post Type
     add_action('init', function () use ($cpt, $cpt_options) {
-        $cpt_plural = str_plural($cpt);
-        $uc_post_type_plural = ucwords(str_replace('-', ' ', $cpt_plural));
-        $uc_post_type_singular = ucwords(str_replace('-', ' ', $cpt));
+        $cptName = str_replace('-', ' ', $cpt);
+        $uc_post_type_plural = Str::of($cptName)->plural()->title();
+        $uc_post_type_singular = Str::of($cptName)->singular()->title();
 
         $labels = array(
-            'name' => _x($uc_post_type_plural, 'Post Type General Name', 'text_domain'),
-            'singular_name' => _x($uc_post_type_singular, 'Post Type Singular Name', 'text_domain'),
-            'menu_name' => __($uc_post_type_plural, 'text_domain'),
-            'name_admin_bar' => __($uc_post_type_singular, 'text_domain'),
+            'name' => _x("{$uc_post_type_plural}", 'Post Type General Name', 'text_domain'),
+            'singular_name' => _x("{$uc_post_type_singular}", 'Post Type Singular Name', 'text_domain'),
+            'menu_name' => __("{$uc_post_type_plural}", 'text_domain'),
+            'name_admin_bar' => __("{$uc_post_type_singular}", 'text_domain'),
             'archives' => __("{$uc_post_type_singular} Archives", 'text_domain'),
             'attributes' => __("{$uc_post_type_singular} Attributes", 'text_domain'),
             'parent_item_colon' => __("Parent {$uc_post_type_singular}:", 'text_domain'),
@@ -110,11 +111,12 @@ foreach ($cpts as $cpt => $cpt_options) {
             'remove_featured_image' => __('Remove featured image', 'text_domain'),
             'use_featured_image' => __('Use as featured image', 'text_domain'),
             'insert_into_item' => __("Insert into {$cpt}", 'text_domain'),
-            'uploaded_to_this_item' => __("Uploaded to this {$cpt}", 'text_domain'),
+            'uploaded_to_this_item' => __("Uploaded to this {$uc_post_type_singular}", 'text_domain'),
             'items_list' => __("{$uc_post_type_plural} list", 'text_domain'),
             'items_list_navigation' => __("{$uc_post_type_plural} list navigation", 'text_domain'),
-            'filter_items_list' => __("Filter {$cpt_plural} list", 'text_domain'),
+            'filter_items_list' => __("Filter {$uc_post_type_plural}", 'text_domain'),
         );
+
         $args = array(
             'label' => __('post_type', 'text_domain'),
             'description' => __($cpt, 'text_domain'),
@@ -125,11 +127,14 @@ foreach ($cpts as $cpt => $cpt_options) {
             'show_in_nav_menus' => true,
             'show_in_admin_bar' => true,
             'supports' => $cpt_options['supports'],
-            'taxonomies' => $cpt_options['taxonomies'],
+            'taxonomies' => $cpt_options['taxonomies'] ?? [],
             'menu_position' => 5,
             'can_export' => true,
             'has_archive' => true,
-            'rewrite' => array('with_front' => false),
+            'rewrite' => [
+                // 'slug' => 'case-studies',
+                'with_front' => false,
+            ],
 
             // --- PUBLIC OPTIONS ---
             'exclude_from_search' => isset($cpt_options['public']) ? $cpt_options['public'] : false,
@@ -138,6 +143,11 @@ foreach ($cpts as $cpt => $cpt_options) {
             'public' => isset($cpt_options['public']) ? $cpt_options['public'] : true,
             'publicly_queryable' => isset($cpt_options['public']) ? $cpt_options['public'] : true,
         );
+
+        if (array_key_exists('has_archive', $cpt_options)) {
+            $args['has_archive'] = $cpt_options['has_archive'];
+        }
+
         register_post_type($cpt, $args);
     }, 0);
 }
